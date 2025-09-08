0dc621eb795f46c5a0c69e48efb0daf6aec34d17
renofour <renofourhi@gmail.com>
2025-09-08 13:37:39 +0900

    Update lightbox.js

--- lightbox.js
+++ lightbox.js
@@ -2,26 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const lightbox = document.getElementById('lightbox');
   const lightboxImg = document.getElementById('lightbox-img');
   const lightboxClose = document.getElementById('lightboxClose');
-  const lightboxContainer = document.querySelector('.lightbox-container');
 
   window.addEventListener('message', function(event) {
     if (event.data.type === 'openLightbox') {
       lightboxImg.src = event.data.src;
       lightbox.style.display = 'block';
-
-      // Reset classes
-      lightboxContainer.classList.remove('landscape', 'portrait');
-
-      // Wait for the image to load
-      lightboxImg.onload = function() {
-        const isLandscape = this.naturalWidth > this.naturalHeight;
-        if (isLandscape) {
-          lightboxContainer.classList.add('landscape');
-        } else {
-          lightboxContainer.classList.add('portrait');
-        }
-      };
-
       const imagesContainer = document.querySelector('.lightbox-images-container');
       if (imagesContainer) {
         imagesContainer.scrollTop = 0;
